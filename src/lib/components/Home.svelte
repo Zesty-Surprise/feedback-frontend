b17cd29a155f0ab21d23083d3424162b7bb2bd9f
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-11-13 14:10:33 +0100

    added css file

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,8 +1,13 @@
 <script>
-  import { Avatar } from '@skeletonlabs/skeleton';
+  import { Avatar } from "@skeletonlabs/skeleton";
 </script>
+
 <body data-theme="skeleton">
   <main>
-    <Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-32" rounded="rounded-full" />
-</main>
+    <Avatar
+      src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop"
+      width="w-32"
+      rounded="rounded-full"
+    />
+  </main>
 </body>
