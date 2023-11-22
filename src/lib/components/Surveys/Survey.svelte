6956551d7c5e59e3a8e995467e2eeda10b5e45f8
Triggero <stoi0003@hz.nl>
2023-11-22 17:53:45 +0100

    create new survey

--- src/lib/components/Surveys/Survey.svelte
+++ src/lib/components/Surveys/Survey.svelte
@@ -3,14 +3,12 @@
     export let date:string = "";
 </script>
 
-<div class="flex flex-col min-w-fit	min-h-fit mr-16 mt-16 hover:scale-105 duration-100">
+<a href="/" class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100">
     <div class="h-52 w-52 bg-[#F5E5D3] rounded-lg">
-        <div>
-
-        </div>
+        <slot/>
     </div>
-    <div>
+    <div class="pl-1">
         <p class="text-[#A19F9F] font-light">{date}</p>
         <p class="text-[#de896e] font-semibold">{title}</p>
     </div>
-</div>
+</a>
--- src/lib/components/Surveys/Surveys.svelte
+++ src/lib/components/Surveys/Surveys.svelte
@@ -35,7 +35,14 @@
     },
   ];
 </script>
-<div class="flex flex-wrap justify-between ml-12 mr-16">
+<div class="flex flex-wrap ml-12 mr-16">
+    <Survey title="Create a new survey">
+        <div class="flex justify-center items-center h-full w-full">
+            <div class="bg-[#de896e] rounded-full w-14 h-14 flex justify-center items-center">
+                <iconify-icon icon="typcn:plus" width=48px/>
+            </div>
+        </div>
+    </Survey>
     {#each surveys as survey}
         <Survey title={survey.title} date={survey.date}/>
     {/each}
