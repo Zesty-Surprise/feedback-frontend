f4d501f44077b7556283057ea730b8c4df5dc8c4
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-11-27 11:27:05 +0100

    made button for add template clickable

--- src/lib/components/Templates.svelte
+++ src/lib/components/Templates.svelte
@@ -1,4 +1,7 @@
 <script>
+  function click() {
+    console.log("you clicked");
+  }
   //javascript an array of objs on
   let templates = [
     {
@@ -73,6 +76,78 @@
         },
       ],
     },
+    {
+      _id: "6556081117d0c3e1240a25dc",
+      name: "TEST-CASE2",
+      date_created: "2023-11-16T12:16:17.924580Z",
+      date_updated: null,
+      components: [
+        {
+          type: "enps-component",
+          custom_text: "",
+          default_text: "Fill in the score!",
+        },
+        {
+          type: "department-component",
+          custom_text: "",
+          default_text: "Select your department!",
+        },
+      ],
+    },
+    {
+      _id: "6556081117d0c3e1240a25dc",
+      name: "TEST-CASE2",
+      date_created: "2023-11-16T12:16:17.924580Z",
+      date_updated: null,
+      components: [
+        {
+          type: "enps-component",
+          custom_text: "",
+          default_text: "Fill in the score!",
+        },
+        {
+          type: "department-component",
+          custom_text: "",
+          default_text: "Select your department!",
+        },
+      ],
+    },
+    {
+      _id: "6556081117d0c3e1240a25dc",
+      name: "TEST-CASE2",
+      date_created: "2023-11-16T12:16:17.924580Z",
+      date_updated: null,
+      components: [
+        {
+          type: "enps-component",
+          custom_text: "",
+          default_text: "Fill in the score!",
+        },
+        {
+          type: "department-component",
+          custom_text: "",
+          default_text: "Select your department!",
+        },
+      ],
+    },
+    {
+      _id: "6556081117d0c3e1240a25dc",
+      name: "TEST-CASE2",
+      date_created: "2023-11-16T12:16:17.924580Z",
+      date_updated: null,
+      components: [
+        {
+          type: "enps-component",
+          custom_text: "",
+          default_text: "Fill in the score!",
+        },
+        {
+          type: "department-component",
+          custom_text: "",
+          default_text: "Select your department!",
+        },
+      ],
+    },
   ];
 </script>
 
@@ -80,16 +155,16 @@
 <div class="grid grid-cols-4 gap-10 mt-10 px-10">
   <div class="bg-white shadow-md rounded-md text-center h-96">
     <div class="h-full flex items-center justify-center">
-      <div
-        class="custom-color rounded-full h-12 w-12 flex items-center justify-center"
-      >
-        <span class="text-white font-bold text-2xl">+</span>
-      </div>
+      <a href="#" on:click={click}>
+        <div
+          class="custom-color rounded-full h-12 w-12 flex items-center justify-center"
+        >
+          <span class="text-white font-bold text-2xl">+</span>
+        </div>
+      </a>
     </div>
     <p class="text-center text-lg font-bold text-black">Add Template</p>
   </div>
-
-  <!-- Text outside the card -->
 
   {#each templates as template}
     <div class="bg-white shadow-md rounded-md text-center h-96">
