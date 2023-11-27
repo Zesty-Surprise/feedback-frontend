7e42c9de81121d6380d96b6b78d9d15f7f57f4e1
Triggero <stoi0003@hz.nl>
2023-11-27 22:09:09 +0100

    create survey modal & form layout

--- src/lib/components/Surveys/Survey.svelte
+++ src/lib/components/Surveys/Survey.svelte
@@ -2,14 +2,27 @@
     export let title:string = "";
     export let date:string = "";
     export let href:string = "";
+    export let isLink:boolean = false;
 </script>
 
-<a href={href} class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100">
-    <div class="h-52 w-52 bg-[#F5E5D3] rounded-lg">
-        <slot/>
+{#if isLink}
+    <a href={href} class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100">
+        <div class="h-52 w-52 bg-[#F5E5D3] rounded-lg">
+            <slot/>
+        </div>
+        <div class="pl-1">
+            <p class="text-[#A19F9F] font-light">{date}</p>
+            <p class="text-[#de896e] font-semibold">{title}</p>
+        </div>
+    </a>
+{:else}
+    <div class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100">
+        <div class="h-52 w-52 bg-[#F5E5D3] rounded-lg">
+            <slot/>
+        </div>
+        <div class="pl-1">
+            <p class="text-[#A19F9F] font-light">{date}</p>
+            <p class="text-[#de896e] font-semibold">{title}</p>
+        </div>
     </div>
-    <div class="pl-1">
-        <p class="text-[#A19F9F] font-light">{date}</p>
-        <p class="text-[#de896e] font-semibold">{title}</p>
-    </div>
-</a>
+{/if}
--- src/routes/surveys/+page.svelte
+++ src/routes/surveys/+page.svelte
@@ -1,6 +1,103 @@
 <script lang="ts">
-  import Surveys from "$lib/components/Surveys/Surveys.svelte";
+  import Modal from "$lib/components/Generic/Modal.svelte";
+  import Survey from "$lib/components/Surveys/Survey.svelte";
+
+
+  const surveys = [
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+  {
+    title: "eNPS Survey",
+    date: "19.10.2023",
+    id: "123122213",
+  },
+];
+
+let showModal = false;
+let options = [
+  'eNPS',
+  'bar',
+  'baz',
+  'bar',
+  'baz',
+  'bar',
+  'baz',
+];
+let selected = options[0];
+
+function handleSubmit(){
+  console.log("submit")
+}
 </script>
 
-<!-- website body -->
-<Surveys />
+<Modal bind:showModal>
+<p slot="header" class="text-[#595959] font-semibold">
+  Create a new survey
+</p>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="flex flex-col mt-8 ml-8 mr-8 gap-6 text-[#595959]">
+    <div class="flex flex-row justify-between items-center">
+      <label>Title</label>
+      <input class="w-2/4 h-8 rounded-md border border-[#2b21181a]" type="text"/>
+    </div>
+    <div class="flex flex-row justify-between items-center">
+      <label>Participants</label>
+      <input class="w-2/4 h-8 rounded-md border border-[#2b21181a]" type="text" />
+    </div>
+    <div class="flex flex-row justify-between items-center">
+      <label>Type</label>
+      <select class="w-2/4 rounded-md border border-[#2b21181a] pt-1 pb-1" bind:value={selected}>
+        {#each options as value}<option class="bg-[#fcf8f4]" {value}>{value}</option>{/each}
+      </select>
+    </div>
+    <div class="pt-3 pb-2 self-center">
+      <button class="text-white bg-[#de896e] hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center" type="submit" on:click={() => (showModal = false)}>
+        Create
+      </button>
+    </div>
+  </div>
+  
+</form>
+</Modal>
+
+<div class="flex flex-wrap ml-12 mr-16">
+  <Survey title="Create a new survey">
+      <button class="flex justify-center items-center h-full w-full" on:click={() => (showModal = true)}>
+        <div class="bg-[#de896e] rounded-full w-14 h-14 flex justify-center items-center">
+          <iconify-icon icon="typcn:plus" width=48px/>
+        </div>
+      </button>
+  </Survey>
+  {#each surveys as survey}
+      <Survey title={survey.title} date={survey.date} isLink href={"/surveys/"+survey.id}/>
+  {/each}
+</div>
