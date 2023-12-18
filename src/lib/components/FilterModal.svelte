59f383129d282996c05a67203b977d1efe601e12
AlexGit123 <70524253+AlexGit123@users.noreply.github.com>
2023-12-18 14:13:04 +0100

    changed the TabWidth of my prettier extension to avoid conflicts ;)

--- src/lib/components/FilterModal.svelte
+++ src/lib/components/FilterModal.svelte
@@ -1,198 +1,208 @@
 <script lang="ts">
-  export let showModal: Boolean;
+    export let showModal: Boolean;
 
-  import { createEventDispatcher } from "svelte";
-  import Modal from "$lib/components/Generic/Modal.svelte";
+    import { createEventDispatcher } from "svelte";
+    import Modal from "$lib/components/Generic/Modal.svelte";
 
-  let checkedCheckboxes: String;
-  const dispatch = createEventDispatcher();
+    let checkedCheckboxes: String;
+    const dispatch = createEventDispatcher();
 
-  function handleCheckbox() {
-    dispatch("checkboxesChanged", checkedCheckboxes);
-  }
+    function handleCheckbox() {
+        dispatch("checkboxesChanged", checkedCheckboxes);
+    }
 
-  function setModal() {
-    showModal = !showModal;
-  }
+    function setModal() {
+        showModal = !showModal;
+    }
 
-  let showSurveyModal = true;
+    let showSurveyModal = true;
 </script>
 
 <Modal bind:showModal={showSurveyModal}>
-  <div>
     <div>
-      <div>
-        <div class="pt-5">
-          <h1 class="pl-5 font-semibold">Filter by team</h1>
-          <h2></h2>
+        <div>
+            <div>
+                <div class="pt-5">
+                    <h1 class="pl-5 font-semibold">Filter by team</h1>
+                    <h2></h2>
+                </div>
+                <div class="pl-16 pt-10 grid grid-cols-2">
+                    <div>
+                        <input
+                            type="radio"
+                            class=""
+                            name="it"
+                            value="IT"
+                            id="it"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="it" class="label-spacing">IT</label>
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="production"
+                            value="Production"
+                            id="production"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="production" class="label-spacing"
+                            >Production</label
+                        >
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="people"
+                            value="People"
+                            id="people"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="people" class="label-spacing">People</label>
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="campus"
+                            value="Campus"
+                            id="campus"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="campus" class="label-spacing">Campus</label>
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="finance"
+                            value="Finance"
+                            id="finance"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="finance" class="label-spacing"
+                            >Finance</label
+                        >
+                    </div>
+
+                    <div>
+                        <input
+                            type="radio"
+                            class=""
+                            name="gifts"
+                            value="Gifts"
+                            id="gifts"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="gifts" class="label-spacing">Gifts</label>
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="innovation"
+                            value="Innovation"
+                            id="innovation"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="innovation" class="label-spacing"
+                            >Innovation</label
+                        >
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="ecommerce"
+                            value="E-commerce"
+                            id="ecommerce"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="ecommerce" class="label-spacing"
+                            >E-commerce</label
+                        >
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="customerService"
+                            value="Customer Service"
+                            id="customerService"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="customerService" class="label-spacing"
+                            >Customer service</label
+                        >
+
+                        <input
+                            type="radio"
+                            class=""
+                            name="purchase"
+                            value="Purchase"
+                            id="purchase"
+                            bind:group={checkedCheckboxes}
+                        />
+                        <label for="purchase" class="label-spacing"
+                            >Purchase</label
+                        >
+                    </div>
+                </div>
+                <div class="flex justify-center space-x-32 pt-5">
+                    <button
+                        class="text-white bg-gray-300 hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
+                        on:click={setModal}>Cancel</button
+                    >
+                    <button
+                        class="text-white bg-color-accent hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
+                        on:click={() => {
+                            setModal(), handleCheckbox();
+                        }}>Apply</button
+                    >
+                </div>
+            </div>
         </div>
-        <div class="pl-16 pt-10 grid grid-cols-2">
-          <div>
-            <input
-              type="radio"
-              class=""
-              name="it"
-              value="IT"
-              id="it"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="it" class="label-spacing">IT</label>
-
-            <input
-              type="radio"
-              class=""
-              name="production"
-              value="Production"
-              id="production"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="production" class="label-spacing">Production</label>
-
-            <input
-              type="radio"
-              class=""
-              name="people"
-              value="People"
-              id="people"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="people" class="label-spacing">People</label>
-
-            <input
-              type="radio"
-              class=""
-              name="campus"
-              value="Campus"
-              id="campus"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="campus" class="label-spacing">Campus</label>
-
-            <input
-              type="radio"
-              class=""
-              name="finance"
-              value="Finance"
-              id="finance"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="finance" class="label-spacing">Finance</label>
-          </div>
-
-          <div>
-            <input
-              type="radio"
-              class=""
-              name="gifts"
-              value="Gifts"
-              id="gifts"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="gifts" class="label-spacing">Gifts</label>
-
-            <input
-              type="radio"
-              class=""
-              name="innovation"
-              value="Innovation"
-              id="innovation"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="innovation" class="label-spacing">Innovation</label>
-
-            <input
-              type="radio"
-              class=""
-              name="ecommerce"
-              value="E-commerce"
-              id="ecommerce"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="ecommerce" class="label-spacing">E-commerce</label>
-
-            <input
-              type="radio"
-              class=""
-              name="customerService"
-              value="Customer Service"
-              id="customerService"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="customerService" class="label-spacing"
-              >Customer service</label
-            >
-
-            <input
-              type="radio"
-              class=""
-              name="purchase"
-              value="Purchase"
-              id="purchase"
-              bind:group={checkedCheckboxes}
-            />
-            <label for="purchase" class="label-spacing">Purchase</label>
-          </div>
-        </div>
-        <div class="flex justify-center space-x-32 pt-5">
-          <button
-            class="text-white bg-gray-300 hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
-            on:click={setModal}>Cancel</button
-          >
-          <button
-            class="text-white bg-color-accent hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
-            on:click={() => {
-              setModal(), handleCheckbox();
-            }}>Apply</button
-          >
-        </div>
-      </div>
     </div>
-  </div>
 </Modal>
 
 <style>
-  .label-spacing {
-    margin-bottom: 15px; /* Adjust the value as needed */
-  }
+    .label-spacing {
+        margin-bottom: 15px; /* Adjust the value as needed */
+    }
 
-  input[type="radio"] {
-    display: none;
-  }
+    input[type="radio"] {
+        display: none;
+    }
 
-  label {
-    cursor: pointer;
-    display: flex;
-    position: relative;
-    padding-left: 35px;
-    align-items: center;
-  }
+    label {
+        cursor: pointer;
+        display: flex;
+        position: relative;
+        padding-left: 35px;
+        align-items: center;
+    }
 
-  label:before {
-    background-color: white;
-    border-radius: 5px;
-    border: 3px solid black;
-    content: "";
-    display: block; /* Change from inline-block to block */
-    height: 36px;
-    left: -25px;
-    width: 36px;
-    position: absolute;
-  }
+    label:before {
+        background-color: white;
+        border-radius: 5px;
+        border: 3px solid black;
+        content: "";
+        display: block; /* Change from inline-block to block */
+        height: 36px;
+        left: -25px;
+        width: 36px;
+        position: absolute;
+    }
 
-  label:after {
-    background-color: transparent;
-    color: black;
-    content: "\2714";
-    font-family: "Arial", sans-serif;
-    font-size: 32px;
-    left: -8px;
-    position: absolute;
-    top: 10px;
-    transform: translate(-50%, -50%) scale(0);
-    transition: transform 0.3s ease;
-  }
+    label:after {
+        background-color: transparent;
+        color: black;
+        content: "\2714";
+        font-family: "Arial", sans-serif;
+        font-size: 32px;
+        left: -8px;
+        position: absolute;
+        top: 10px;
+        transform: translate(-50%, -50%) scale(0);
+        transition: transform 0.3s ease;
+    }
 
-  input:checked + label:after {
-    transform: translate(-50%, -50%) scale(1);
-  }
+    input:checked + label:after {
+        transform: translate(-50%, -50%) scale(1);
+    }
 </style>
--- src/routes/dashboard/templates/+page.svelte
+++ src/routes/dashboard/templates/+page.svelte
@@ -1,125 +1,134 @@
 <script lang="ts">
-  import { goto, invalidateAll } from "$app/navigation";
-  import { fetchAPI } from "$lib/functions";
-  import Modal from "$lib/components/Generic/Modal.svelte";
+    import { goto, invalidateAll } from "$app/navigation";
+    import { fetchAPI } from "$lib/functions";
+    import Modal from "$lib/components/Generic/Modal.svelte";
 
-  export let data: any;
+    export let data: any;
 
-  let showDeleteModal = false;
-  let selectedForDelete = "";
+    let showDeleteModal = false;
+    let selectedForDelete = "";
 
-  const toggleDelete = (id: string) => {
-    selectedForDelete === ""
-      ? (selectedForDelete = id)
-      : (selectedForDelete = "");
-    showDeleteModal = !showDeleteModal;
-  };
+    const toggleDelete = (id: string) => {
+        selectedForDelete === ""
+            ? (selectedForDelete = id)
+            : (selectedForDelete = "");
+        showDeleteModal = !showDeleteModal;
+    };
 
-  const deleteRequest = async (id: string): Promise<void> => {
-    const res = await fetchAPI(`templates/${id}`, "DELETE", data.cookie);
-    const json = await res.json();
-    return json;
-  };
+    const deleteRequest = async (id: string): Promise<void> => {
+        const res = await fetchAPI(`templates/${id}`, "DELETE", data.cookie);
+        const json = await res.json();
+        return json;
+    };
 
-  const deleteTemplate = async (): Promise<void> => {
-    const res = await deleteRequest(selectedForDelete);
-    toggleDelete("");
-    invalidateAll();
-  };
+    const deleteTemplate = async (): Promise<void> => {
+        const res = await deleteRequest(selectedForDelete);
+        toggleDelete("");
+        invalidateAll();
+    };
 </script>
 
 <Modal bind:showModal={showDeleteModal}>
-  <p slot="header" class="text-color-text font-semibold text-center">
-    Are you sure you want to delete this template?
-  </p>
-  <div class="pt-3 pb-2 self-center flex justify-center gap-5">
-    <button
-      class="text-white bg-red-500 hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
-      type="submit"
-      on:click={() => deleteTemplate()}
-    >
-      Delete
-    </button>
-    <button
-      class="text-white bg-gray-300 hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
-      type="submit"
-      on:click={() => toggleDelete("")}
-    >
-      Cancel
-    </button>
-  </div>
+    <p slot="header" class="text-color-text font-semibold text-center">
+        Are you sure you want to delete this template?
+    </p>
+    <div class="pt-3 pb-2 self-center flex justify-center gap-5">
+        <button
+            class="text-white bg-red-500 hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
+            type="submit"
+            on:click={() => deleteTemplate()}
+        >
+            Delete
+        </button>
+        <button
+            class="text-white bg-gray-300 hover:brightness-90 transition duration-200 focus:outline-none font-medium rounded-full text-md px-6 py-2 text-center"
+            type="submit"
+            on:click={() => toggleDelete("")}
+        >
+            Cancel
+        </button>
+    </div>
 </Modal>
 
 <div class="relative mt-10 px-10 text-zinc-600">
-  <h1 class="block text-xl font-medium">Template collection</h1>
-  <p>Add, Edit and Delete survey templates here</p>
+    <h1 class="block text-xl font-medium">Template collection</h1>
+    <p>Add, Edit and Delete survey templates here</p>
 </div>
 <div class="flex flex-wrap ml-10 mr-16">
-  <div
-    class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100"
-  >
     <div
-      class="flex items-center justify-evenly h-52 w-52 bg-color-highlight rounded-lg"
+        class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100"
     >
-      <button
-        class="flex justify-center items-center h-full w-full"
-        on:click={() => goto(`/dashboard/templates/builder`)}
-      >
         <div
-          class="bg-color-accent rounded-full w-14 h-14 flex justify-center items-center"
+            class="flex items-center justify-evenly h-52 w-52 bg-color-highlight rounded-lg"
         >
-          <iconify-icon class="text-white" icon="typcn:plus" width="48px" />
+            <button
+                class="flex justify-center items-center h-full w-full"
+                on:click={() => goto(`/dashboard/templates/builder`)}
+            >
+                <div
+                    class="bg-color-accent rounded-full w-14 h-14 flex justify-center items-center"
+                >
+                    <iconify-icon
+                        class="text-white"
+                        icon="typcn:plus"
+                        width="48px"
+                    />
+                </div>
+            </button>
         </div>
-      </button>
+        <div class="pl-1">
+            <p class="text-color-accent font-semibold">Create New Template</p>
+        </div>
     </div>
-    <div class="pl-1">
-      <p class="text-color-accent font-semibold">Create New Template</p>
-    </div>
-  </div>
-  {#each [...data.templates].reverse() as template}
-    {@const date = new Date(template.date_created).toLocaleDateString()}
-    <div
-      class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100"
-    >
-      <div
-        class="flex items-center justify-evenly h-52 w-52 bg-color-highlight rounded-lg"
-      >
-        <button
-          class="bg-color-accent rounded-2xl w-14 h-14 flex justify-center items-center hover:brightness-90 transition duration-200"
-          on:click={() => goto(`/dashboard/templates/${template._id}`)}
+    {#each [...data.templates].reverse() as template}
+        {@const date = new Date(template.date_created).toLocaleDateString()}
+        <div
+            class="flex flex-col min-w-fit min-h-fit mr-16 mt-16 hover:scale-105 duration-100"
         >
-          <iconify-icon
-            class="text-white"
-            icon="mingcute:inspect-line"
-            width="40px"
-          />
-        </button>
-        <button
-          class="bg-color-accent rounded-2xl w-14 h-14 flex justify-center items-center hover:brightness-90 transition duration-200"
-          on:click={() => {
-            toggleDelete(template._id);
-          }}
-        >
-          <iconify-icon class="text-white" icon="ph:trash-fill" width="40px" />
-        </button>
-      </div>
-      <div class="pl-1">
-        <p class="text-color-text_light font-light">{date}</p>
-        <p class="text-color-accent font-semibold">{template.name}</p>
-      </div>
-    </div>
-  {/each}
+            <div
+                class="flex items-center justify-evenly h-52 w-52 bg-color-highlight rounded-lg"
+            >
+                <button
+                    class="bg-color-accent rounded-2xl w-14 h-14 flex justify-center items-center hover:brightness-90 transition duration-200"
+                    on:click={() =>
+                        goto(`/dashboard/templates/${template._id}`)}
+                >
+                    <iconify-icon
+                        class="text-white"
+                        icon="mingcute:inspect-line"
+                        width="40px"
+                    />
+                </button>
+                <button
+                    class="bg-color-accent rounded-2xl w-14 h-14 flex justify-center items-center hover:brightness-90 transition duration-200"
+                    on:click={() => {
+                        toggleDelete(template._id);
+                    }}
+                >
+                    <iconify-icon
+                        class="text-white"
+                        icon="ph:trash-fill"
+                        width="40px"
+                    />
+                </button>
+            </div>
+            <div class="pl-1">
+                <p class="text-color-text_light font-light">{date}</p>
+                <p class="text-color-accent font-semibold">{template.name}</p>
+            </div>
+        </div>
+    {/each}
 </div>
 
 <style>
-  .custom-color {
-    background-color: #de896e;
-  }
-  .text-color {
-    color: #de896e;
-  }
-  .border-custom {
-    border: 2px solid #de896e;
-    border-radius: 25px;
-  }
+    .custom-color {
+        background-color: #de896e;
+    }
+    .text-color {
+        color: #de896e;
+    }
+    .border-custom {
+        border: 2px solid #de896e;
+        border-radius: 25px;
+    }
 </style>
